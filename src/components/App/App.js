7eaa1a0193d5a552ffe43be43140a4a7b84a0d85
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-11-19 20:32:14 -0700

    Set up JSX and imports in App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,26 @@
+import React from "react";
 import "./App.css";
+import SearchBar from "../SearchBar";
+import SearchResults from "../SearchResults";
+import Playlist from "../Playlist";
 
-function App() {
-  return <div className="App">Jamming web app!</div>;
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

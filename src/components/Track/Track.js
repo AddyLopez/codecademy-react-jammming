408966cacd0941075cd388efc8ffaea35aac90cc
AddyLopez <101231208+AddyLopez@users.noreply.github.com>
2022-11-19 21:02:13 -0700

    Add renderAction method to Track class component

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -3,6 +3,14 @@ import React from "react";
 import "./Track.css";
 
 class Track extends React.Component {
+  renderAction() {
+    if (isRemoval) {
+      return <button className="Track-action">-</button>;
+    } else {
+      return <button className="Track-action">+</button>;
+    }
+  }
+
   render() {
     return (
       <div className="Track">
